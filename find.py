9026fcb261cb1bc9fa02de900dd24e815e3f7a92
Kyle Bell <kyle@localhost.localdomain>
2018-02-27 09:22:38 -0600

    Modified so that the program can display an image infront of the green objects 	new file:   completedexample.jpg 	modified:   find.py

--- find.py
+++ find.py
@@ -75,17 +75,17 @@ def findEdge(startRow, numRows, numCols, image, shapes):
 
 img = cv.imread('green3.jpg')
 img = cv.resize(img,None,fx=0.25,fy=0.25, interpolation = cv.INTER_AREA)
-#owl = cv.imread('owl.jpg')
+owl = cv.imread('owl.jpg')
 greenObjects = []
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV )
-#owlhsv = cv.cvtColor(owl, cv.COLOR_BGR2HSV)
+owlhsv = cv.cvtColor(owl, cv.COLOR_BGR2HSV)
 lower = np.array([40, 150, 72])
 upper = np.array([90, 255, 255])
 
 mask = cv.inRange(hsv,lower,upper)
-#owlMask = cv.inRange(owlhsv,lower,upper)
+owlMask = cv.inRange(owlhsv,lower,upper)
 maskInv = cv.bitwise_not(mask,mask=None)
-#owlMaskInv = cv.bitwise_not(owlMask,mask=None)
+owlMaskInv = cv.bitwise_not(owlMask,mask=None)
 
 #numRows, numCols = maskInv.shape[:2]
 numRows, numCols = mask.shape[:2]
@@ -101,28 +101,38 @@ for i in range(numRows):
             maskImage.append(mask[greenObjects[count].minRow:greenObjects[count].maxRow,greenObjects[count].minCol:greenObjects[count].maxCol])
             maskInvImage.append(maskInv[greenObjects[count].minRow:greenObjects[count].maxRow,greenObjects[count].minCol:greenObjects[count].maxCol])
             count = count + 1
-title = 'Green_Image_'
-number = 0
-for i in image:
-    cv.imshow(title + str(number), i)
-    cv.imwrite(title + str(number) + ".jpg", i)
-    number += 1
+#title = 'Green_Image_'
+#number = 0
+#for i in image:
+#    cv.imshow(title + str(number), i)
+#    cv.imwrite(title + str(number) + ".jpg", i)
+#    number += 1
+totalRows =[]
+totalCols = []
+owlRows, owlCols = owl.shape[:2]
+owlImages = []
+owlMaskList = []
+owlMaskInvList = []
+for i in range(len(image)):
+    totalRows = (image[i].shape[0])
+    totalCols = (image[i].shape[1])
+    scaleR = (totalRows[i]/float(owlRows))
+    scaleC = (totalCols[i]/float(owlCols))
+    r = owlRows*scaleR
+    c = owlCols*scaleC
+    owlImages.append(cv.resize(owl,(int(c),int(r))))
+    owlMaskList.append(cv.resize(owlMask,(int(c),int(r))))
+    owlMaskInvList.append(cv.resize(owlMaskInv,(int(c),int(r))))
+    owlImages[i] = cv.bitwise_and(owlImages[i],owlImages[i],mask=owlMaskInvList[i])
+    image[i] = cv.bitwise_and(image[i],image[i],mask=owlMaskList[i])
+    image[i] = cv.add(image[i],owlImages[i])
 
 
+for i in range(len(greenObjects)):
+    img[greenObjects[i].minRow:greenObjects[i].maxRow,greenObjects[i].minCol:greenObjects[i].maxCol] = image[i]
 
+cv.imshow("image", img)
+cv.imwrite("completedexample.jpg", img)
 
-
-
-
-#This will be the basis of my code to show an image in front of the green object
-#owl = cv.bitwise_and(owl,owl,mask=owlMaskInv)
-#owlRows, owlCols = owl.shape[:2]
-#imgPiece = img[:owlRows,:owlCols]
-#imgPiece = cv.bitwise_and(imgPiece,imgPiece,mask=owlMask)
-#imgPiece = cv.add(owl,imgPiece)
-#img[:owlRows,:owlCols] = imgPiece
-#cv.imshow('imgPiece',img)
-#img3 = cv.add(owl,owlMaskInv)
-#cv.imshow('test',owl)
 cv.waitKey(0)
 cv.destroyAllWindows()
